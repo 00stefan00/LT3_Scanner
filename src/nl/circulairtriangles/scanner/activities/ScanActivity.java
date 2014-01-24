238e8f44a87396e655aa3bd642e97ee47954e9ae
vincentvandeemter <vincent.vandeemter@gmail.com>
2014-01-24 15:23:02 +0100

    Scanner toegevoegd met de kracht van duizend zonnen. Ascii penis.:

--- src/nl/circulairtriangles/scanner/activities/ScanActivity.java
+++ src/nl/circulairtriangles/scanner/activities/ScanActivity.java
@@ -1,9 +1,15 @@
 package nl.circulairtriangles.scanner.activities;
 
 import nl.circulairtriangles.scanner.R;
+import nl.circulairtriangles.scanner.utils.IntentIntegrator;
+import nl.circulairtriangles.scanner.utils.IntentResult;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class ScanActivity extends BaseActivity {
 
@@ -12,6 +18,8 @@ public class ScanActivity extends BaseActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_scan);
 		initLayout(R.string.title_activity_scanner, true, false, true, false);
+		IntentIntegrator integrator = new IntentIntegrator(this);
+		integrator.initiateScan();
 	}
 
 	@Override
@@ -20,5 +28,19 @@ public class ScanActivity extends BaseActivity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		  IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+		  if (scanResult != null) {
+		    // handle scan result
+			  Log.e("<test>", scanResult.toString());
+			  Context context = getApplicationContext();
+			  CharSequence text = scanResult.getContents();
+			  int duration = Toast.LENGTH_LONG;
 
+			  Toast toast = Toast.makeText(context, text, duration);
+			  toast.show();
+		  }
+		  // else continue with any other code you need in the method
+		}
 }
